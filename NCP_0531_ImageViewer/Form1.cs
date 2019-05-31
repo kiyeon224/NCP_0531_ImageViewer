181e351bbd9ba48579c3b1382413acfcdfe1cd8b
kiyeon224 <kiyeon224@naver.com>
2019-05-31 11:48:00 +0900

    #2 PictureBox, imageList 컨트롤을 사용하여 사진첩 코드 구현

--- NCP_0531_ImageViewer/Form1.cs
+++ NCP_0531_ImageViewer/Form1.cs
@@ -11,9 +11,39 @@ namespace NCP_0531_ImageViewer
 {
     public partial class Form1 : Form
     {
+        int imgCount = 0;  //현재 보고있는 이미지 번호
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.picImg.Image = (Image)this.imgList.Images[this.imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            this.imgCount--;
+            if (this.imgCount < 0)
+            {
+                this.imgCount = this.imgList.Images.Count - 1;
+            }
+            this.picImg.Image = (Image)this.imgList.Images[this.imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            this.imgCount++;
+            if (this.imgCount > 42)
+            {
+                this.imgCount = 0;
+            }
+            this.picImg.Image = (Image)this.imgList.Images[this.imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
         }
     }
 }
